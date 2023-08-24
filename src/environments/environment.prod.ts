b528a0c496d9db66d2085a5e4833a81dbd9614f8
Luis Henrique <101938473+Luis-Henrique@users.noreply.github.com>
2023-08-24 11:23:22 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   url_reload: 'https://mango-plant-0ce961a0f.3.azurestaticapps.net/#/login',
-  url_login: 'https://docmanager-api.azurewebsites.net/api/v1/Account',
-  url_api: 'https://docmanager-api.azurewebsites.net/api/v1/',
+  url_login: 'https://luishenriquefnx-001-site1.btempurl.com/api/v1/Account',
+  url_api: 'https://luishenriquefnx-001-site1.btempurl.com/api/v1/',
   /*
   url_login: 'https://luishenriquefnx-001-site1.btempurl.com/api/v1/Account',
   url_api: 'https://luishenriquefnx-001-site1.btempurl.com/api/v1/',
